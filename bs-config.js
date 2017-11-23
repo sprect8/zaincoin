fa3410d846130b4ad92d2a48f60419ac2b905eb2
sprect8 <sprect8@gmail.com>
2017-11-23 10:01:44 +0800

    disable browser sync

--- bs-config.js
+++ bs-config.js
@@ -3,6 +3,7 @@ module.exports = {
     "server": {
       "baseDir": ["./src", "./build/contracts"]
     },
-    "port": process.env.PORT || 3000
+    "port": process.env.PORT || 3000,
+    "open" : false
   }
   
